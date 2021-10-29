09f73c6b838896dbb4bdad3607a66061fb3d01ba
Sumanth Burla <sumanth.burla@dxc.com>
2021-10-29 12:54:51 +0530

    v1.1

--- loginAPI.php
+++ loginAPI.php
@@ -4,10 +4,10 @@ require_once("include/curl_call.php");
 
 $email=@$_POST['email'];
 $password=@$_POST['pass'];
-echo $email;
+// echo $email,$password;
     // $passs=md5($password);
     // login by mail
-            $url = 'https://trafficadmin.herokuapp.com/api/validLogin.php/';
+            $url = 'http://35.225.127.184/api/validLogin.php/';
             
             $content = json_encode(array('Email'=>$email,'Password'=>$password),JSON_FORCE_OBJECT);
            
@@ -15,16 +15,16 @@ echo $email;
             $response=curlcall($url,$content);
             
             // $response returns...
-            var_dump($response);
-            echo $response;
+            // var_dump($response);
+            //echo $response;
             if($response['message'] == 'Invalid Email')
             {
-            header('location:login.php?error=User doesn\'t existssss');
+            header('location:index.php?error=User doesn\'t existssss');
             die();
             }
-            if($response['message'] == 'Invalid Password!!')
+            if($response['message'] == 'Invalid Password!')
             {
-            header('location:login.php?error=Invalid Password !');
+            header('location:index.php?error=Invalid Password !');
             die();
             }           
             if($response['message'] == 'ValidLogin')
